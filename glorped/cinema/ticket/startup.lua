19174e604c59a83704ff884dcb867536f4c6cc25
kamiicode <77517394+kamiicode@users.noreply.github.com>
2026-05-17 20:28:08 -0300

    abananaaaaa

--- glorped/cinema/ticket/startup.lua
+++ glorped/cinema/ticket/startup.lua
@@ -1,1 +1,13 @@
+local input_inventory = peripheral.find("minecraft:barrel")
+local output_inventory = peripheral.find("minecraft:chest")
 
+while true do
+  for i=1, input_inventory.size() do
+    if barrel.getItemDetail(i).name == "minecraft:slime" then
+        input_inventory.pushItems(output_inventory.name, 1)
+    else
+        print(i)
+    end
+  end
+  sleep(1)
+end
