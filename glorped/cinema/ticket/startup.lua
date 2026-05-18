5bf92b4bfa37fd3acbfa1eed10085aad37e35256
kamiicode <77517394+kamiicode@users.noreply.github.com>
2026-05-17 23:47:34 -0300

    banbana

--- glorped/cinema/ticket/startup.lua
+++ glorped/cinema/ticket/startup.lua
@@ -3,10 +3,13 @@ local output_inventory = peripheral.find("minecraft:chest")
 
 while true do
   for slot, item in pairs(input_inventory.list()) do
-    if input_inventory.getItemDetail(slot).name == "minecraft:slime" then
-        input_inventory.pushItems(output_inventory.name, 1)
+    if input_inventory.getItemDetail(slot).name == "minecraft:slime_ball" then
+        input_inventory.pushItems(peripheral.getName(output_inventory), slot)
+        redstone.setOutput("left", true)
+        sleep(3*item.count)
+        redstone.setOutput("left", false)
     else
-        print(slot, item)
+        print(slot, item.name)
     end
   end
   sleep(1)
